fc100258b340b90fca096e6e4cb4a960e0e62460
Jim Chiang <jchiang@slac.stanford.edu>
2016-07-26 16:48:35 -0700

    sorting out the interface based on wl-workflow-test example

--- python/desc/workflow_engine/workflow_engine.py
+++ python/desc/workflow_engine/workflow_engine.py
@@ -1,11 +1,83 @@
-class workflow_engine(object):
-    '''
-    Example class for workflow_engine package.
-    '''
-    def __init__(self, message):
-        self.message = message
+_indent = ' '*4
 
-    def run(self, raise_error=False):
-        if raise_error:
-            raise RuntimeError()
-        return self.message
+def job():
+    return '<job maxCPU="${MAXCPU}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
+
+def long_job():
+    return '<job maxCPU="${MAXCPULONG}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
+
+class Pipeline(object):
+    def __init__(self, name):
+        self.main_task = Task(name)
+
+    def __str__(self):
+        return str(self.main_task)
+
+class Task(object):
+    def __init__(self, name):
+        self.name = name
+        self.processes = {}
+
+    def set_variables(self, vars):
+        for key, value in vars:
+            self.set_variable(key, value)
+
+    def set_variable(self, key, value):
+        pass
+
+    def add_process(self, process):
+        self.processes[process.name] = process
+        if process.superTask is not None:
+            raise RuntimeError("Cannot assign a process to more than one task")
+        process.supertask = self.name
+
+    def __str__(self):
+        lines = [str(process) for process in self.processes.values()]
+        return '\n'.join(lines)
+
+class Process(object):
+    def __init__(self, name):
+        self.name = name
+        self.notation = None
+        self.job = None
+        self.script = None
+        self.requirements = []
+        self.subtasks = []
+        self.supertask = None
+
+    def requires(self, process):
+        self.requirements.append(process)
+
+    def add_subtask(self, task):
+        self.subtasks.append(task)
+
+    def __str__(self):
+        lines = []
+        lines.append('<process name="%s" site="${JOBSITE}">' % self.name)
+        if self.notation is not None:
+            lines.append(_indent + '<notation>' + self.notation '</notation>')
+        # A process comprises either a job or a script.
+        if self.job is not None:
+            lines.append(_indent + self.job)
+        else:
+            lines.append(_indent + '<script><![CDATA[')
+            lines.append('  execfile("%s/%s" % (SLAC_SCRIPT_LOCATION, SCRIPT_NAME))')
+            lines.append('  %s()' % self.name)
+            lines.append(_indent + '</script>')
+        if self.requirements:
+            lines.append(_indent + '<depends>')
+            for process in self.requirements:
+                if process.supertask is not None:
+                    process_name = '.'.join((process.supertask, process.name))
+                else:
+                    process_name = process.name
+                lines.append(2*_indent +
+                             ('<after process="%s"/>' % process_name))
+            lines.append(_indent + '</depends>')
+        if self.subtasks:
+            lines.append(_indent + '<createsSubtasks>')
+            for subtask in self.subtasks:
+                lines.append(2*_indent + ('<subtask>%s</subtask>' % subtask))
+            lines.append(_indent + '</createsSubtasks>')
+        lines.append('</process>')
+        return '\n'.join(lines)
--- tests/test_workflow_engine.py
+++ tests/test_workflow_engine.py
@@ -1,19 +1,35 @@
 """
-Example unit tests for workflow_engine package
+Unit tests for workflow engine xml generator code.
 """
 import unittest
-import desc.workflow_engine
+import desc.workflow_engine.workflow_engine as engine
 
-class workflow_engineTestCase(unittest.TestCase):
+pipeline = engine.Pipeline('WLPipeline')
+main_task = pipeline.main_task
+main_task.set_variables(variables)
+
+catalog_selection = engine.Process('catalogSelection')
+main_task.add_process(catalog_selection)
+
+setup_cat_sel_NTs = engine.Process('setupCatalogSelectionNullTests')
+setup_cat_sel_NTs.requires(catalog_selection)
+
+cat_sel_NT_task = engine.Task('catalogSelectionNullTestsTask')
+setup_cat_sel_NTs.add_subtask(cat_sel_NT_task)
+cat_sel_null_test = engine.Process('catalogSelectionNullTest')
+cat_sel_NT_task.add_process(cat_sel_null_test)
+
+class WorkflowEngineTestCase(unittest.TestCase):
     def setUp(self):
-        self.message = 'Hello, world'
-        
+        pass
+
     def tearDown(self):
         pass
 
-    def test_run(self):
-        foo = desc.workflow_engine.workflow_engine(self.message)
-        self.assertEquals(foo.run(), self.message)
+    def test_SubTaskInterface(self):
+        subtask = self.generator.taskFactory(task_name)
+        process = self.generator.processFactory(process_name)
+        subtask.add_process(process)
 
     def test_failure(self):
         self.assertRaises(TypeError, desc.workflow_engine.workflow_engine)
