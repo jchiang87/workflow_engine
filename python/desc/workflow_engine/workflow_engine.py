f03ff867cba90460b33a90456883af3e9b595248
Jim Chiang <jchiang@slac.stanford.edu>
2016-07-26 21:50:38 -0700

    more refactoring: add Task.create_[parallel_]process methods

--- python/desc/workflow_engine/workflow_engine.py
+++ python/desc/workflow_engine/workflow_engine.py
@@ -1,4 +1,13 @@
+from collections import OrderedDict
+from xml.dom import minidom
+
+__all__ = ['job', 'long_job', 'Pipeline', 'Task', 'Process']
+
 _indent = ' '*4
+
+def check_name(name):
+    if len(name) > 30:
+        raise ValueError('Process or task name must be 30 characters or fewer')
 
 def job():
     return '<job maxCPU="${MAXCPU}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
@@ -7,16 +16,20 @@ def long_job():
     return '<job maxCPU="${MAXCPULONG}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
 
 class Pipeline(object):
-    def __init__(self, name):
-        self.main_task = Task(name)
+    def __init__(self, name, version='0.1'):
+        self.main_task = Task(name, version=version)
 
     def __str__(self):
         return str(self.main_task)
 
 class Task(object):
-    def __init__(self, name):
+    def __init__(self, name, version=None):
+        check_name(name)
         self.name = name
-        self.processes = {}
+        self.version = version
+        self.notation = None
+        self.variables = []
+        self.processes = OrderedDict()
 
     def set_variables(self, vars):
         for key, value in vars:
@@ -27,12 +40,39 @@ class Task(object):
 
     def add_process(self, process):
         self.processes[process.name] = process
-        if process.superTask is not None:
+        if process.supertask is not None:
             raise RuntimeError("Cannot assign a process to more than one task")
-        process.supertask = self.name
+        if self.version is None:
+            process.supertask = self.name
+
+    def create_process(self, process_name, job=None, requirements=[]):
+        process = Process(process_name)
+        process.job = job
+        self.add_process(process)
+        return process
+
+    def create_parallel_process(self, process_name, job, requirements=[]):
+        setup_process = Process('setup_' + process_name + 's')
+        self.add_process(setup_process)
+        for process in requirements:
+            setup_process.requires(process)
+        subtask = Task(process_name + 'sTask')
+        setup_process.add_subtask(subtask)
+        inner_process = Process(process_name)
+        inner_process.job = job
+        subtask.add_process(inner_process)
+        return inner_process
 
     def __str__(self):
-        lines = [str(process) for process in self.processes.values()]
+        lines = []
+        if self.version is not None:
+            lines.append('<task name="%s" type="LSST" version="%s">'
+                         % (self.name, self.version))
+        else:
+            lines.append('<task name="%s" type="LSST">' % self.name)
+        lines.extend(self.variables)
+        lines.extend([str(process) for process in self.processes.values()])
+        lines.append('</task>')
         return '\n'.join(lines)
 
 class Process(object):
@@ -55,14 +95,15 @@ class Process(object):
         lines = []
         lines.append('<process name="%s" site="${JOBSITE}">' % self.name)
         if self.notation is not None:
-            lines.append(_indent + '<notation>' + self.notation '</notation>')
-        # A process comprises either a job or a script.
+            lines.append('%s<notation>%s</notation>' % (_indent, self.notation))
+        # A process comprises only either a job or a script.
         if self.job is not None:
             lines.append(_indent + self.job)
         else:
             lines.append(_indent + '<script><![CDATA[')
             lines.append('  execfile("%s/%s" % (SLAC_SCRIPT_LOCATION, SCRIPT_NAME))')
             lines.append('  %s()' % self.name)
+            lines.append(']]>')
             lines.append(_indent + '</script>')
         if self.requirements:
             lines.append(_indent + '<depends>')
@@ -77,7 +118,10 @@ class Process(object):
         if self.subtasks:
             lines.append(_indent + '<createsSubtasks>')
             for subtask in self.subtasks:
-                lines.append(2*_indent + ('<subtask>%s</subtask>' % subtask))
+                lines.append(2*_indent + ('<subtask>%s</subtask>'
+                                          % subtask.name))
             lines.append(_indent + '</createsSubtasks>')
         lines.append('</process>')
+        if self.subtasks:
+            lines.extend([str(subtask) for subtask in self.subtasks])
         return '\n'.join(lines)
--- tests/test_workflow_engine.py
+++ tests/test_workflow_engine.py
@@ -1,40 +1,69 @@
 """
 Unit tests for workflow engine xml generator code.
 """
+from xml.dom import minidom
 import unittest
 import desc.workflow_engine.workflow_engine as engine
 
 pipeline = engine.Pipeline('WLPipeline')
 main_task = pipeline.main_task
-main_task.set_variables(variables)
+#main_task.set_variables(variables)
 
-catalog_selection = engine.Process('catalogSelection')
-main_task.add_process(catalog_selection)
+catsel = main_task.create_process('catalogSelection', engine.job())
+catsel_nt = main_task.create_parallel_process('catalogSelectionNullTest',
+                                              job=engine.job(),
+                                              requirements=[catsel])
 
-setup_cat_sel_NTs = engine.Process('setupCatalogSelectionNullTests')
-setup_cat_sel_NTs.requires(catalog_selection)
+pz = main_task.create_parallel_process('photoZCharacterization',
+                                       job=engine.job(),
+                                       requirements=[catsel_nt])
+pz_nt = main_task.create_parallel_process('photoZCharNullTest',
+                                          job=engine.job(),
+                                          requirements=[pz])
 
-cat_sel_NT_task = engine.Task('catalogSelectionNullTestsTask')
-setup_cat_sel_NTs.add_subtask(cat_sel_NT_task)
-cat_sel_null_test = engine.Process('catalogSelectionNullTest')
-cat_sel_NT_task.add_process(cat_sel_null_test)
+tomo_bin = main_task.create_process('tomographicBinning', job=engine.job(),
+                                    requirements=[pz_nt])
+tomo_bin_nt = main_task.create_parallel_process('tomoBinningNullTest',
+                                                job=engine.job(),
+                                                requirements=[tomo_bin])
 
-class WorkflowEngineTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
+dndz_inf = main_task.create_process('dNdzInference', job=engine.job(),
+                                    requirements=[tomo_bin_nt, pz_nt])
+dndz_inf_nt = main_task.create_parallel_process('dNdzInferenceNullTest',
+                                                job=engine.job(),
+                                                requirements=[dndz_inf])
 
-    def tearDown(self):
-        pass
+tpcf = main_task.create_process('2PCFEstimate', job=engine.job(),
+                                requirements=[tomo_bin_nt])
+tpcf_nt = main_task.create_parallel_process('2PCFEstimateNullTest',
+                                            job=engine.job(),
+                                            requirements=[tpcf])
 
-    def test_SubTaskInterface(self):
-        subtask = self.generator.taskFactory(task_name)
-        process = self.generator.processFactory(process_name)
-        subtask.add_process(process)
+cov_model = main_task.create_process('covarianceModel', job=engine.job(),
+                                     requirements=[tpcf_nt])
+cov_model_nt = main_task.create_parallel_process('covarianceModelNullTest',
+                                                 job=engine.job(),
+                                                 requirements=[cov_model])
+tjp_cosmo = main_task.create_process('TJPCosmo',job=engine.job(),
+                                     requirements=[cov_model_nt, tpcf_nt,
+                                                   dndz_inf_nt])
 
-    def test_failure(self):
-        self.assertRaises(TypeError, desc.workflow_engine.workflow_engine)
-        foo = desc.workflow_engine.workflow_engine(self.message)
-        self.assertRaises(RuntimeError, foo.run, True)
-
-if __name__ == '__main__':
-    unittest.main()
+#class WorkflowEngineTestCase(unittest.TestCase):
+#    def setUp(self):
+#        pass
+#
+#    def tearDown(self):
+#        pass
+#
+#    def test_SubTaskInterface(self):
+#        subtask = self.generator.taskFactory(task_name)
+#        process = self.generator.processFactory(process_name)
+#        subtask.add_process(process)
+#
+#    def test_failure(self):
+#        self.assertRaises(TypeError, desc.workflow_engine.workflow_engine)
+#        foo = desc.workflow_engine.workflow_engine(self.message)
+#        self.assertRaises(RuntimeError, foo.run, True)
+#
+#if __name__ == '__main__':
+#    unittest.main()
