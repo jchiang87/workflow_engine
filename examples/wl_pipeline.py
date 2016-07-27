dcfbec189febfb174c75233c4eae3d5934197392
Jim Chiang <jchiang@slac.stanford.edu>
2016-07-27 09:57:13 -0700

    create an example for the weak lensing pipeline; more refactoring

---
+++ examples/wl_pipeline.py
@@ -0,0 +1,69 @@
+"""
+Script to generate xml for weak lensing tomographic binning pipeline.
+See https://confluence.slac.stanford.edu/display/LSSTDESC/WL+Tomography+Pipeline+Hack
+"""
+import desc.workflow_engine.workflow_engine as engine
+
+pipeline = engine.Pipeline('JC_WLPipeline', '0.1')
+main_task = pipeline.main_task
+main_task.notation = 'Weak Lensing Pipeline for Cosmological Parameter Estimation'
+
+# Set the main task variables from the data/main_task_variables.txt
+# configuration file.
+main_task.set_variables()
+
+# Catalog selection process (and null tests).
+catsel = main_task.create_process('catalogSelection')
+catsel.notation = 'Make selections on the DM Catalog data'
+catsel_nt = main_task.create_parallel_process('catalogSelectionNullTest',
+                                              requirements=[catsel])
+
+# Photo-z characterization.
+pz = main_task.create_parallel_process('photoZCharacterization',
+                                       requirements=[catsel_nt])
+pz.owner_task.setup_process.notation = 'Photo-z characterization task'
+pz_nt = main_task.create_parallel_process('photoZCharNullTest',
+                                          requirements=[pz])
+
+# Tomographic binning of catalog data.
+tomo_bin = main_task.create_process('tomographicBinning',
+                                    requirements=[pz_nt])
+tomo_bin.notation = 'Tomographic binning of catalog selections'
+tomo_bin_nt = main_task.create_parallel_process('tomoBinningNullTest',
+                                                requirements=[tomo_bin])
+
+# N(z) inference from tomographic binning and P(z).
+dndz_inf = main_task.create_process('dNdzInference',
+                                    requirements=[tomo_bin_nt, pz_nt])
+dndz_inf.notation = 'Inference of N(z)'
+dndz_inf_nt = main_task.create_parallel_process('dNdzInferenceNullTest',
+                                                requirements=[dndz_inf])
+
+# Two-point correlation function estimation.
+tpcf = main_task.create_process('2PCFEstimate',
+                                requirements=[tomo_bin_nt])
+tpcf.notation = '2-point correlation function estimation'
+tpcf_nt = main_task.create_parallel_process('2PCFEstimateNullTest',
+                                            requirements=[tpcf])
+
+# Covariance matrix calculation.
+cov_model = main_task.create_process('covarianceModel',
+                                     requirements=[tpcf_nt])
+cov_model.notation = 'Covariance matrix calculation'
+cov_model_nt = main_task.create_parallel_process('covarianceModelNullTest',
+                                                 requirements=[cov_model])
+
+# Joint probes cosmological infererence.
+tjp_cosmo = main_task.create_process('TJPCosmo',
+                                     requirements=[cov_model_nt, tpcf_nt,
+                                                   dndz_inf_nt])
+tjp_cosmo.notation = 'Joint probe cosomlogical parameter estimation'
+
+# Write the xml to an output file.
+with open('wl_pipeline.xml', 'w') as output:
+    output.write(pipeline.toxml() + '\n')
+
+# Create the python module with the substream launching functions for
+# the parallelized subtasks.  The module name is determined from the
+# main task variables.
+#pipeline.write_python_model()
--- python/desc/workflow_engine/workflow_engine.py
+++ python/desc/workflow_engine/workflow_engine.py
@@ -8,12 +8,9 @@ def check_name(name):
     if len(name) > 30:
         raise ValueError('Process or task name must be 30 characters or fewer')
 
-def job_line(job_type):
-    if job_type is None:
-        return None
-    if job_type == 'long':
-        return '<job maxCPU="${MAXCPULONG}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
-    return '<job maxCPU="${MAXCPU}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
+job_line = dict([('script', None),
+                 ('long', '<job maxCPU="${MAXCPULONG}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'),
+                 ('std', '<job maxCPU="${MAXCPU}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>')])
 
 def data_path(filename):
     return os.path.join(os.environ['WORKFLOW_ENGINE_DIR'], 'data', filename)
@@ -43,6 +40,7 @@ class Task(object):
         self.name = name
         self.version = version
         self.notation = None
+        self.setup_process = None
         self.variable_lines = []
         self.processes = OrderedDict()
 
@@ -54,19 +52,22 @@ class Task(object):
 
     def add_process(self, process):
         self.processes[process.name] = process
-        if process.supertask is not None:
+        if process.owner_task is not None:
             raise RuntimeError("Cannot assign a process to more than one task")
         if self.version is None:
-            process.supertask = self.name
+            process.owner_task = self
 
-    def create_process(self, process_name, job_type=None, requirements=[]):
+    def create_process(self, process_name, job_type='std', requirements=[]):
         process = Process(process_name)
-        process.job = job_line(job_type)
+        process.job = job_line[job_type]
         self.add_process(process)
+        for item in requirements:
+            process.requires(item)
         return process
 
-    def create_parallel_process(self, process_name, job_type, requirements=[]):
-        if job_type is None:
+    def create_parallel_process(self, process_name, job_type='std',
+                                requirements=[]):
+        if job_type not in ('std', 'long'):
             raise ValueError("job_type for a parallel process must be 'long' or 'std'")
         setup_process = Process('setup_' + process_name + 's')
         self.add_process(setup_process)
@@ -75,7 +76,7 @@ class Task(object):
         subtask = Task(process_name + 'sTask')
         setup_process.add_subtask(subtask)
         inner_process = Process(process_name)
-        inner_process.job = job_line(job_type)
+        inner_process.job = job_line[job_type]
         subtask.add_process(inner_process)
         return inner_process
 
@@ -107,13 +108,14 @@ class Process(object):
         self.script = None
         self.requirements = []
         self.subtasks = []
-        self.supertask = None
+        self.owner_task = None
 
     def requires(self, process):
         self.requirements.append(process)
 
     def add_subtask(self, task):
         self.subtasks.append(task)
+        task.setup_process = self
 
     def _script_lines(self):
         lines = []
@@ -129,8 +131,9 @@ class Process(object):
         if self.requirements:
             lines.append('<depends>')
             for process in self.requirements:
-                if process.supertask is not None:
-                    process_name = '.'.join((process.supertask, process.name))
+                if process.owner_task is not None:
+                    process_name = '.'.join((process.owner_task.name,
+                                             process.name))
                 else:
                     process_name = process.name
                 lines.append(('<after process="%s"/>' % process_name))
@@ -153,7 +156,7 @@ class Process(object):
         lines = []
         lines.append('<process name="%s" site="${JOBSITE}">' % self.name)
         if self.notation is not None:
-            lines.append('%s<notation>%s</notation>' % (_indent, self.notation))
+            lines.append('<notation>%s</notation>' % (self.notation))
         # A process comprises only either a job or a script.
         if self.job is not None:
             lines.append(self.job)
