12a51bce9b6090364b937e9bf01e555c6c79e25c
Jim Chiang <jchiang@slac.stanford.edu>
2016-07-27 08:17:17 -0700

    read main task variables and pipeline header boilerplate from text files; refactor __str__ methods

--- python/desc/workflow_engine/workflow_engine.py
+++ python/desc/workflow_engine/workflow_engine.py
@@ -1,26 +1,41 @@
+import os
 from collections import OrderedDict
 from xml.dom import minidom
 
-__all__ = ['job', 'long_job', 'Pipeline', 'Task', 'Process']
-
-_indent = ' '*4
+__all__ = ['Pipeline', 'Task', 'MainTask', 'Process']
 
 def check_name(name):
     if len(name) > 30:
         raise ValueError('Process or task name must be 30 characters or fewer')
 
-def job():
+def job_line(job_type):
+    if job_type is None:
+        return None
+    if job_type == 'long':
+        return '<job maxCPU="${MAXCPULONG}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
     return '<job maxCPU="${MAXCPU}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
 
-def long_job():
-    return '<job maxCPU="${MAXCPULONG}" batchOptions="${BATCH_OPTIONS}" executable="${SCRIPT_LOCATION}/${BATCH_NAME}"/>'
+def data_path(filename):
+    return os.path.join(os.environ['WORKFLOW_ENGINE_DIR'], 'data', filename)
 
 class Pipeline(object):
-    def __init__(self, name, version='0.1'):
-        self.main_task = Task(name, version=version)
+    def __init__(self, name, version, pipeline_header=None):
+        self.main_task = MainTask(name, version)
+        self._read_pipeline_header(pipeline_header)
+
+    def _read_pipeline_header(self, pipeline_header):
+        if pipeline_header is None:
+            pipeline_header = data_path('slac_pipeline_header.txt')
+        with open(pipeline_header) as input_:
+            self.header = ''.join(input_.readlines()).strip()
 
     def __str__(self):
-        return str(self.main_task)
+        lines = [self.header, str(self.main_task), '</pipeline>']
+        return '\n'.join(lines)
+
+    def toxml(self, encoding='UTF-8', newl='', indent=4*' '):
+        doc = minidom.parseString(str(self))
+        return doc.toprettyxml(encoding=encoding, newl=newl, indent=indent)
 
 class Task(object):
     def __init__(self, name, version=None):
@@ -28,15 +43,14 @@ class Task(object):
         self.name = name
         self.version = version
         self.notation = None
-        self.variables = []
+        self.variable_lines = []
         self.processes = OrderedDict()
 
-    def set_variables(self, vars):
-        for key, value in vars:
-            self.set_variable(key, value)
-
-    def set_variable(self, key, value):
-        pass
+    def set_variables(self, varfile=None):
+        if varfile is None:
+            varfile = data_path('main_task_variables.txt')
+        with open(varfile) as input_:
+            self.variable_lines = [x.strip() for x in input_]
 
     def add_process(self, process):
         self.processes[process.name] = process
@@ -45,13 +59,15 @@ class Task(object):
         if self.version is None:
             process.supertask = self.name
 
-    def create_process(self, process_name, job=None, requirements=[]):
+    def create_process(self, process_name, job_type=None, requirements=[]):
         process = Process(process_name)
-        process.job = job
+        process.job = job_line(job_type)
         self.add_process(process)
         return process
 
-    def create_parallel_process(self, process_name, job, requirements=[]):
+    def create_parallel_process(self, process_name, job_type, requirements=[]):
+        if job_type is None:
+            raise ValueError("job_type for a parallel process must be 'long' or 'std'")
         setup_process = Process('setup_' + process_name + 's')
         self.add_process(setup_process)
         for process in requirements:
@@ -59,21 +75,29 @@ class Task(object):
         subtask = Task(process_name + 'sTask')
         setup_process.add_subtask(subtask)
         inner_process = Process(process_name)
-        inner_process.job = job
+        inner_process.job = job_line(job_type)
         subtask.add_process(inner_process)
         return inner_process
 
+    def _header_lines(self):
+        return ['<task name="%s" type="LSST">' % self.name]
+
     def __str__(self):
         lines = []
-        if self.version is not None:
-            lines.append('<task name="%s" type="LSST" version="%s">'
-                         % (self.name, self.version))
-        else:
-            lines.append('<task name="%s" type="LSST">' % self.name)
-        lines.extend(self.variables)
+        lines.extend(self._header_lines())
+        lines.extend(self.variable_lines)
         lines.extend([str(process) for process in self.processes.values()])
         lines.append('</task>')
         return '\n'.join(lines)
+
+class MainTask(Task):
+    def __init__(self, name, version):
+        super(MainTask, self).__init__(name)
+        self.version = version
+
+    def _header_lines(self):
+        return ['<task name="%s" type="LSST" version="%s">'
+                % (self.name, self.version)]
 
 class Process(object):
     def __init__(self, name):
@@ -91,6 +115,40 @@ class Process(object):
     def add_subtask(self, task):
         self.subtasks.append(task)
 
+    def _script_lines(self):
+        lines = []
+        lines.append('<script><![CDATA[')
+        lines.append('    execfile("%s/%s" % (SLAC_SCRIPT_LOCATION, SCRIPT_NAME))')
+        lines.append('    %s()' % self.name)
+        lines.append(']]>')
+        lines.append('</script>')
+        return lines
+
+    def _requirements_lines(self):
+        lines = []
+        if self.requirements:
+            lines.append('<depends>')
+            for process in self.requirements:
+                if process.supertask is not None:
+                    process_name = '.'.join((process.supertask, process.name))
+                else:
+                    process_name = process.name
+                lines.append(('<after process="%s"/>' % process_name))
+            lines.append('</depends>')
+        return lines
+
+    def _subtask_lines(self):
+        lines = []
+        if self.subtasks:
+            lines.append('<createsSubtasks>')
+            for subtask in self.subtasks:
+                lines.append('<subtask>%s</subtask>' % subtask.name)
+            lines.append('</createsSubtasks>')
+        lines.append('</process>')
+        if self.subtasks:
+            lines.extend([str(subtask) for subtask in self.subtasks])
+        return lines
+
     def __str__(self):
         lines = []
         lines.append('<process name="%s" site="${JOBSITE}">' % self.name)
@@ -98,30 +156,9 @@ class Process(object):
             lines.append('%s<notation>%s</notation>' % (_indent, self.notation))
         # A process comprises only either a job or a script.
         if self.job is not None:
-            lines.append(_indent + self.job)
+            lines.append(self.job)
         else:
-            lines.append(_indent + '<script><![CDATA[')
-            lines.append('  execfile("%s/%s" % (SLAC_SCRIPT_LOCATION, SCRIPT_NAME))')
-            lines.append('  %s()' % self.name)
-            lines.append(']]>')
-            lines.append(_indent + '</script>')
-        if self.requirements:
-            lines.append(_indent + '<depends>')
-            for process in self.requirements:
-                if process.supertask is not None:
-                    process_name = '.'.join((process.supertask, process.name))
-                else:
-                    process_name = process.name
-                lines.append(2*_indent +
-                             ('<after process="%s"/>' % process_name))
-            lines.append(_indent + '</depends>')
-        if self.subtasks:
-            lines.append(_indent + '<createsSubtasks>')
-            for subtask in self.subtasks:
-                lines.append(2*_indent + ('<subtask>%s</subtask>'
-                                          % subtask.name))
-            lines.append(_indent + '</createsSubtasks>')
-        lines.append('</process>')
-        if self.subtasks:
-            lines.extend([str(subtask) for subtask in self.subtasks])
+            lines.extend(self._script_lines())
+        lines.extend(self._requirements_lines())
+        lines.extend(self._subtask_lines())
         return '\n'.join(lines)
--- tests/test_workflow_engine.py
+++ tests/test_workflow_engine.py
@@ -1,52 +1,53 @@
 """
 Unit tests for workflow engine xml generator code.
 """
-from xml.dom import minidom
 import unittest
 import desc.workflow_engine.workflow_engine as engine
 
-pipeline = engine.Pipeline('WLPipeline')
+pipeline = engine.Pipeline('WLPipeline', '0.4')
 main_task = pipeline.main_task
-#main_task.set_variables(variables)
+main_task.set_variables()
 
-catsel = main_task.create_process('catalogSelection', engine.job())
+catsel = main_task.create_process('catalogSelection', job_type='std')
 catsel_nt = main_task.create_parallel_process('catalogSelectionNullTest',
-                                              job=engine.job(),
+                                              job_type='std',
                                               requirements=[catsel])
 
 pz = main_task.create_parallel_process('photoZCharacterization',
-                                       job=engine.job(),
+                                       job_type='std',
                                        requirements=[catsel_nt])
 pz_nt = main_task.create_parallel_process('photoZCharNullTest',
-                                          job=engine.job(),
+                                          job_type='std',
                                           requirements=[pz])
 
-tomo_bin = main_task.create_process('tomographicBinning', job=engine.job(),
+tomo_bin = main_task.create_process('tomographicBinning', job_type='std',
                                     requirements=[pz_nt])
 tomo_bin_nt = main_task.create_parallel_process('tomoBinningNullTest',
-                                                job=engine.job(),
+                                                job_type='std',
                                                 requirements=[tomo_bin])
 
-dndz_inf = main_task.create_process('dNdzInference', job=engine.job(),
+dndz_inf = main_task.create_process('dNdzInference', job_type='std',
                                     requirements=[tomo_bin_nt, pz_nt])
 dndz_inf_nt = main_task.create_parallel_process('dNdzInferenceNullTest',
-                                                job=engine.job(),
+                                                job_type='std',
                                                 requirements=[dndz_inf])
 
-tpcf = main_task.create_process('2PCFEstimate', job=engine.job(),
+tpcf = main_task.create_process('2PCFEstimate', job_type='std',
                                 requirements=[tomo_bin_nt])
 tpcf_nt = main_task.create_parallel_process('2PCFEstimateNullTest',
-                                            job=engine.job(),
+                                            job_type='std',
                                             requirements=[tpcf])
 
-cov_model = main_task.create_process('covarianceModel', job=engine.job(),
+cov_model = main_task.create_process('covarianceModel', job_type='std',
                                      requirements=[tpcf_nt])
 cov_model_nt = main_task.create_parallel_process('covarianceModelNullTest',
-                                                 job=engine.job(),
+                                                 job_type='std',
                                                  requirements=[cov_model])
-tjp_cosmo = main_task.create_process('TJPCosmo',job=engine.job(),
+tjp_cosmo = main_task.create_process('TJPCosmo', job_type='std',
                                      requirements=[cov_model_nt, tpcf_nt,
                                                    dndz_inf_nt])
+
+print pipeline.toxml()
 
 #class WorkflowEngineTestCase(unittest.TestCase):
 #    def setUp(self):
